be1207018e9d7bb7f53e9d3cd57c2c4ce8a419ff
HallowKaz <hollowkaz@gmail.com>
2023-02-10 15:27:22 -0500

    Practicing Python Hashes

--- Two Sum/Two Sum.py
+++ Two Sum/Two Sum.py
@@ -10,5 +10,7 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]. """
 
-print("I can lift you up... I can show you what you want to see and take you where you want to be.. you can be my luck... even in a hurricane you'll find I know that we'll be safe and sound.")
-
+my_dict=dict(Kaz='000')
+your_dict={'Alondra':'000'}
+print(my_dict)
+print(your_dict)
